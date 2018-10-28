b9121195ad1295aac92c84cda1f17c91d67d1cfb
Jonny Banana <azazaelisback@gmail.com>
2018-10-28 22:31:24 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  
+  <title> Barbara Cartomante pagina 404 </title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="css/style404.css" />
+  <link href="https://fonts.googleapis.com/css?family=Ranga" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Architects+Daughter" rel="stylesheet">
+  <!-- modernizr enables HTML5 elements and feature detects -->
+  <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
+  <link type="text/plain" rel="author" href="humans.txt" />
+</head>
+
+<body>
+	<div id="main">
+   
+			<h1> Oops! Qualcosa è andato storto...</h1>
+			<h2> clicca qui per tornare alla Home Page di Barbara </h2>
+		</div>
+    
+		
+			
+	</div>
+		
+  
+ <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100897062-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+
+</html>
